3e22d70d4418f2914175d01ef6ed6b1b47bc5fca
Leooo 3 <leokeene@icloud.com>
2023-12-23 21:47:03 +0000

    added statusText so i can see wtf is wrong with this code

--- taylorizer/script.js
+++ taylorizer/script.js
@@ -1,21 +1,23 @@
+const { response } = require("express");
+
 var backend = "https://mastodon-tender-leopard.ngrok-free.app";
 var queries = window.location.search;
 var params = new URLSearchParams(queries);
 
 async function get_token(URL) {
     var token = await fetch(URL);
-    return token.json();
+    return token;
 };
 
 if (params.has('code') && params.has('state')) {
     spotify_token = get_token(backend + '/callback' + '?code=' + params.get('code') + '&state=' + params.get('state'))
     // get rid of this console.log in production model pls
-    console.log(JSON.stringify(spotify_token))
+    console.log()
     if (spotify_token.status === 200) {
-        console.log("success");
+        console.log(spotify_token.statusText);
         // will come back to this later
     } else {
-        console.log("auth error");
+        console.log(spotify_token.statusText);
         // will also come back to this later
     };
 };
