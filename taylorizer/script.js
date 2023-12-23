3ed977aae8a5c6618fdd14115e0eb42e2e192be6
Leooo 3 <120170440+ctrleo@users.noreply.github.com>
2023-12-23 21:19:57 +0000

    Fixing await
    Smh

--- taylorizer/script.js
+++ taylorizer/script.js
@@ -2,8 +2,13 @@ var backend = "https://mastodon-tender-leopard.ngrok-free.app";
 var queries = window.location.search;
 var params = new URLSearchParams(queries);
 
+async function get_token(URL) {
+    var token = await fetch(URL);
+    return token;
+};
+
 if (params.has('code') && params.has('state')) {
-    spotify_token = await fetch(backend + '/callback' + '?code=' + params.get('code') + '&state=' + params.get('state'))
+    spotify_token = await get_token(backend + '/callback' + '?code=' + params.get('code') + '&state=' + params.get('state'))
     if (spotify_token.status === 200) {
         console.log("success");
         // will come back to this later
