5b10dc139d5f4de6a0811d5146f303d452f49727
Leooo 3 <120170440+ctrleo@users.noreply.github.com>
2023-12-23 12:58:55 +0000

    Added await to fetch
    Don't go chasing waterfalls 🎶

--- taylorizer/script.js
+++ taylorizer/script.js
@@ -2,5 +2,5 @@ var callback = "http://mastodon-tender-leopard.ngrok-free.app/callback";
 var queries = window.location.search;
 var params = new URLSearchParams(queries);
 if (params.has('code') && params.has('state') {
-    spotify_token = fetch(callback + '?code=' params.get('code') + '&state=' + params.get('state');
+    spotify_token = await fetch(callback + '?code=' params.get('code') + '&state=' + params.get('state');
 };
