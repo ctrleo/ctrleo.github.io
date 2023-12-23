ffb96afb59599e965e56b8a747cc6ab5ffc32c16
Leooo 3 <120170440+ctrleo@users.noreply.github.com>
2023-12-23 21:24:01 +0000

    Learned my lesson 😞

--- taylorizer/script.js
+++ taylorizer/script.js
@@ -8,7 +8,7 @@ async function get_token(URL) {
 };
 
 if (params.has('code') && params.has('state')) {
-    spotify_token = await get_token(backend + '/callback' + '?code=' + params.get('code') + '&state=' + params.get('state'))
+    spotify_token = get_token(backend + '/callback' + '?code=' + params.get('code') + '&state=' + params.get('state'))
     if (spotify_token.status === 200) {
         console.log("success");
         // will come back to this later
