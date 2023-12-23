c80829613d79f644f6ee86d82938d25b32ddd712
Leooo 3 <120170440+ctrleo@users.noreply.github.com>
2023-12-23 18:51:26 +0000

    Tried to bypass ngrok

--- taylorizer/script.js
+++ taylorizer/script.js
@@ -1,6 +1,16 @@
 var backend = "http://mastodon-tender-leopard.ngrok-free.app";
 var queries = window.location.search;
 var params = new URLSearchParams(queries);
+
+//ngrok bypass
+fetch(backend, {
+    method: 'POST',
+    headers: {
+        'content-type': 'text/plain',
+        'ngrok-skip-browser-warning': true
+    }
+})
+
 if (params.has('code') && params.has('state') {
     spotify_token = await fetch(backend + '/callback' + '?code=' params.get('code') + '&state=' + params.get('state');
     if (spotify_token.status === 200) {
