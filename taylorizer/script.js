7441d233892eab7dc1e88307b2afcb47011c5b2a
Leooo 3 <leokeene@icloud.com>
2023-12-23 22:53:38 +0000

    added promise.then (also added hopes/dreams)

--- taylorizer/script.js
+++ taylorizer/script.js
@@ -1,13 +1,15 @@
 var backend = "https://mastodon-tender-leopard.ngrok-free.app";
 var queries = window.location.search;
 var params = new URLSearchParams(queries);
+// url = backend + '/callback' + '?code=' + params.get('code') + '&state=' + params.get('state')
 
-async function get_token(URL) {
-    var token = await fetch(URL);
-    return token;
-};
 
 if (params.has('code') && params.has('state')) {
-    spotify_token = get_token(backend + '/callback' + '?code=' + params.get('code') + '&state=' + params.get('state'));
-    console.log(spotify_token.constructor.name);
+    spotify_token = fetch(backend + '/callback' + '?code=' + params.get('code') + '&state=' + params.get('state'));
+    // add loading icon here
+    console.log("loading");
+    spotify_token.then(
+        function() {console.log("got token!!!")},
+        function() {console.log("got token, error returned :(")}
+    )
 };
