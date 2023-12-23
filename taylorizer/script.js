18461ea2e645f72b794bda90518b504f0b918a10
Leooo 3 <leokeene@icloud.com>
2023-12-23 21:36:31 +0000

    (trying) to get token

--- taylorizer/script.js
+++ taylorizer/script.js
@@ -4,11 +4,13 @@ var params = new URLSearchParams(queries);
 
 async function get_token(URL) {
     var token = await fetch(URL);
-    return token;
+    return token.json();
 };
 
 if (params.has('code') && params.has('state')) {
     spotify_token = get_token(backend + '/callback' + '?code=' + params.get('code') + '&state=' + params.get('state'))
+    // get rid of this console.log in production model pls
+    console.log(JSON.stringify(spotify_token))
     if (spotify_token.status === 200) {
         console.log("success");
         // will come back to this later
