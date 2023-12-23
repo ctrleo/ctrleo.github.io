37477cc3c2672382fe95f1d954b95ceb78b9ec14
Leooo 3 <120170440+ctrleo@users.noreply.github.com>
2023-12-23 16:36:15 +0000

    hopefully works
    idek what i did

--- taylorizer/script.js
+++ taylorizer/script.js
@@ -1,17 +1,37 @@
-var headers = new Headers();
-headers.set("ngrok-skip-browser-warning", true)
-var callback = "http://mastodon-tender-leopard.ngrok-free.app/callback";
+/* todo:
+        implement errors
+        add key refresh
+        add loading animation
+*/
+
+var backend = "http://mastodon-tender-leopard.ngrok-free.app";
 var queries = window.location.search;
 var params = new URLSearchParams(queries);
 
+class spotify_token {
+    constructor(access_token, token_type, scope, expires_in, refresh_token) {
+        this.access = access_token;
+        this.type = token_type;
+        this.scope = scope;
+        this.expiry = expires_in;
+        this.refresh = refresh_token;
+    };
+};
+
+
+function getAuthLink() {
+    // because ngrok is mean and wants to scare my users away
+    login = await fetch(backend + "/login");
+    window.open(login, "_self");
+};
 
 if (params.has('code') && params.has('state') {
-    spotify_token = await fetch(callback + '?code=' params.get('code') + '&state=' + params.get('state');
+    var tokens = await fetch(callback + '?code=' params.get('code') + '&state=' + params.get('state');
     if (spotify_token.status === 200) {
-        console.log("success");
-        // will come back to this later
+        var token new spotify_token(tokens.body.access_token, tokens.body.token_type, tokens.body.scope, tokens.body.expires_in, tokens.body.refresh_token);
+        console.log("GOT SPOTIFY AUTH TOKEN")
     } else {
         console.log("auth error");
-        // will also come back to this later
+        // alexa remind me to implement errors
     };
 };
