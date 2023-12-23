9757babf86e8c6a1833a528dbcac3515a5c79789
Leooo 3 <120170440+ctrleo@users.noreply.github.com>
2023-12-23 18:33:22 +0000

    changed backend address
    sorry abt the chaos yall i fixed it

--- taylorizer/script.js
+++ taylorizer/script.js
@@ -1,8 +1,8 @@
-var callback = "http://mastodon-tender-leopard.ngrok-free.app/callback";
+var backend = "http://mastodon-tender-leopard.ngrok-free.app";
 var queries = window.location.search;
 var params = new URLSearchParams(queries);
 if (params.has('code') && params.has('state') {
-    spotify_token = await fetch(callback + '?code=' params.get('code') + '&state=' + params.get('state');
+    spotify_token = await fetch(backend + '/callback' + '?code=' params.get('code') + '&state=' + params.get('state');
     if (spotify_token.status === 200) {
         console.log("success");
         // will come back to this later
