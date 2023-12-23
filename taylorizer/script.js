cb7eaac671b7b41cf6db2f142ee7388b674661ed
Leooo 3 <leokeene@icloud.com>
2023-12-23 18:28:10 +0000

    Revert "Update script.js"
    This reverts commit 6df58f7a60f6efcf2dd69ff16a096bbf93e1c984.
    dw guys i fixed it

---
+++ taylorizer/script.js
@@ -0,0 +1,13 @@
+var callback = "http://mastodon-tender-leopard.ngrok-free.app/callback";
+var queries = window.location.search;
+var params = new URLSearchParams(queries);
+if (params.has('code') && params.has('state') {
+    spotify_token = await fetch(callback + '?code=' params.get('code') + '&state=' + params.get('state');
+    if (spotify_token.status === 200) {
+        console.log("success");
+        // will come back to this later
+    } else {
+        console.log("auth error");
+        // will also come back to this later
+    };
+};
