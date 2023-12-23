6df58f7a60f6efcf2dd69ff16a096bbf93e1c984
Leooo 3 <120170440+ctrleo@users.noreply.github.com>
2023-12-23 15:31:47 +0000

    Update script.js

--- taylorizer/script.js
+++ taylorizer/script.js
@@ -1,6 +1,10 @@
+var headers = new Headers();
+headers.set("ngrok-skip-browser-warning", true)
 var callback = "http://mastodon-tender-leopard.ngrok-free.app/callback";
 var queries = window.location.search;
 var params = new URLSearchParams(queries);
+
+
 if (params.has('code') && params.has('state') {
     spotify_token = await fetch(callback + '?code=' params.get('code') + '&state=' + params.get('state');
     if (spotify_token.status === 200) {
