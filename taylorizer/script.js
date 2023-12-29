37d24ebb21083ff543f0d198268cd37796f17192
Leooo 3 <leokeene@icloud.com>
2023-12-29 21:53:53 +0000

    Made a loading bar 🤩

--- taylorizer/script.js
+++ taylorizer/script.js
@@ -3,8 +3,11 @@ var params = new URLSearchParams(window.location.search);
 
 if (params.has("code")) {
     var token;
+    document.getElementById("sign-in").style.display = "none";
+    document.getElementById("loading").style.display = "inline-block";
     fetch(backend + "/callback?code=" + params.get("code"))
         .then(response => response.text())
         .then(text => {token = text})
         .finally(function() {localStorage.setItem("access_token", token)});
 };
+
