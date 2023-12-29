75427bd73e3b0c560935ede89a9165ff569db47d
Leooo 3 <leokeene@icloud.com>
2023-12-29 13:59:35 +0000

    backend FINALLY works omg

--- taylorizer/script.js
+++ taylorizer/script.js
@@ -1,11 +1,10 @@
 var backend = "http://127.0.0.1:3000";
 var params = new URLSearchParams(window.location.search);
 
-
 if (params.has("code")) {
+    var token;
     fetch(backend + "/callback?code=" + params.get("code"))
-    .then(function(rsp) {if (rsp.ok) {
-        spotify_tokens = rsp.json()
-        .then(function() {window.localStorage.setItem("auth_token", JSON.stringify(spotify_tokens))})
-    }})
+        .then(response => response.text())
+        .then(text => {token = text})
+        .finally(function() {localStorage.setItem("access_token", token)});
 };
