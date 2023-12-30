1bb125656da7528272147aa7bcd412471d2170ad
Leooo 3 <leokeene@icloud.com>
2023-12-30 10:42:39 +0000

    added error text and loading thingy

--- taylorizer/script.js
+++ taylorizer/script.js
@@ -2,18 +2,19 @@ var backend = "https://138.68.116.108";
 var params = new URLSearchParams(window.location.search);
 var origin = window.location.origin
 
-if (params.has("code")) {
-    var token;
-    document.getElementById("sign-in").style.display = "none";
-    document.getElementById("loading").style.display = "inline-block";
-    fetch(backend + "/callback?code=" + params.get("code"))
-        .then(response => response.text())
-        .then(text => {token = text})
-        .catch(function() {window.location.replace(origin + "/?auth_error=true")})
-        .finally(function() {localStorage.setItem("access_token", token)});
+function checkparams() {
+    if (params.has("auth_error")) {
+        document.getElementById("caption").style.color = "red";
+        document.getElementById("caption").innerText = "Auth error occured :/ please try again!"
+    };
+    if (params.has("code")) {
+        var token;
+        document.getElementById("sign-in").style.display = "none";
+        document.getElementById("loading").style.display = "inline-block";
+        fetch(backend + "/callback?code=" + params.get("code"))
+            .then(response => response.text())
+            .then(text => {token = text})
+            .catch(function() {window.location.replace(origin + "?auth_error=true")})
+            .finally(function() {localStorage.setItem("access_token", token)});
+    };
 };
-
-if (params.has("auth_error")) {
-    document.getElementById("caption").style.color = "rgb(255,0,0)";
-    document.getElementById("caption").innerText = "Auth error occured :/ pls try again";
-}
